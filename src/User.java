cdd2dafc5867533a161b4cdf0ab0f47d50857cf8
moxueyi <moxueyi2333@gmail.com>
2024-03-05 10:21:56 +0800

    第一次提交

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+
+    String userName = "userName";
+    String password = "password";
+    String email = "email";
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
